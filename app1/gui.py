85fd640697565c8ff546ff6822dc7da4bdc3ea84
adamabdi <adamabdi145810@gmail.com>
2024-02-22 14:38:30 -0600

    Create basic GUI without interactions, day 16.

--- app1/gui.py
+++ app1/gui.py
@@ -5,5 +5,7 @@ label = sg.Text("Type in to-do item")
 input_box = sg.InputText(tooltip="Enter to-do")
 add_button = sg.Button("Add")
 window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+
 window.read()
+print("Hello World!")
 window.close()
